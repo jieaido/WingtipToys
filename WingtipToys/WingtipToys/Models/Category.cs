f5e34c38a5ad4057f19a3181b0194fd69e8cca90
jieaido <jieaido@163.com>
2016-01-10 21:14:30 +0800

    烦,先不做了

---
+++ WingtipToys/WingtipToys/Models/Category.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace WingtipToys.Models
+{
+    public class Category
+    {
+        [ScaffoldColumn(false)]
+        public int CategoryID { get; set; }
+
+        [Required, StringLength(100), Display(Name = "名称")]
+        public string CategoryName { get; set; }
+
+        [Display(Name = " 描述")]
+        public string Description { get; set; }
+
+        public virtual ICollection<Product> Products { get; set; }
+    }
+}
---
+++ WingtipToys/WingtipToys/Models/Products.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace WingtipToys.Models
+{
+    public class Product
+    {
+        [ScaffoldColumn(false)]
+        public int ProductID { get; set; }
+
+        [Required, StringLength(100), Display(Name = "名称")]
+        public string ProductName { get; set; }
+
+        [Required, StringLength(10000), Display(Name = "产品描述"), DataType(DataType.MultilineText)]
+        public string Description { get; set; }
+
+        public string ImagePath { get; set; }
+
+        [Display(Name = "单价")]
+        public double? UnitPrice { get; set; }
+
+        public int? CategoryID { get; set; }
+
+        public virtual Category Category { get; set; }
+    }
+}
